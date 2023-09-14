9566c2e73653497525464ce47d25ec0e6387ac9c
Ashish Srivastava <fortcollinsashish@gmail.com>
2023-09-14 12:24:22 -0600

    adjusted print statement to include units of emissions

--- print_fires.py
+++ print_fires.py
@@ -6,19 +6,34 @@ def main():
     year_column = 1
     savannah_fires_column = 2
     forest_fires_column = 3
+    organic_soil_fires_column = 22
+    humid_tropical_forest_fires_column = 23
+    
     file_name = 'Agrofood_co2_emission.csv'
     
     savannah_fires_strings_list = my_utils.get_column(file_name, 
                                                       country_column, 
                                                       country, 
                                                       result_column=savannah_fires_column)
-    savannah_fires = sum([float(fire) for fire in savannah_fires_strings_list])
     forest_fires_string_list = my_utils.get_column(file_name, 
                                                    country_column, 
                                                    country, 
-                                                   forest_fires_column)
+                                                   result_column=forest_fires_column)
+    organic_soil_fires_column = my_utils.get_column(file_name,
+                                                    country_column,
+                                                    country,
+                                                    result_column=organic_soil_fires_column)
+    humid_tropical_forest_fires_column = my_utils.get_column(file_name,
+                                                            country_column,
+                                                            country,
+                                                            result_column=humid_tropical_forest_fires_column)
+    
+    savannah_fires = sum([float(fire) for fire in savannah_fires_strings_list])
     forest_fires = sum([float(fire) for fire in forest_fires_string_list])
-    fires = savannah_fires + forest_fires
+    organic_soil_fires = sum([float(fire) for fire in organic_soil_fires_column])
+    humid_tropical_forest_fires = sum([float(fire) for fire in humid_tropical_forest_fires_column])
+    
+    fires = savannah_fires + forest_fires + organic_soil_fires + humid_tropical_forest_fires
     
     years_with_fires_string_list = my_utils.get_column(file_name, 
                                                        country_column, 
@@ -29,7 +44,7 @@ def main():
     minimum_year = min(years_with_fires_list)
     maximum_year = max(years_with_fires_list)
     
-    print(f"There were a total of {fires} savannah and forest fires in {country} from {minimum_year} to {maximum_year}")
+    print(f"There was a total of {fires} kilotons of CO2 emissions from savannah, forest, organic soil, and humid tropical fires in {country} from {minimum_year} to {maximum_year}")
 
 if __name__ == '__main__':
     main()
