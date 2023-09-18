158c23323d364b2590b2cc0bd80b20f161e06e33
Ashish Srivastava <fortcollinsashish@gmail.com>
2023-09-18 13:57:04 -0600

    added error handling for file opening and for type casting

--- my_utils.py
+++ my_utils.py
@@ -19,13 +19,56 @@ def get_column(file_name, query_column, query_value, result_column=1):
         the column of data that matches the query value
     """
     result_column_string_accumulator = []
-    with open(file_name) as f:
-        lines_array = [line.split(',') for line in f]
-        # Skip header column
-        for line in lines_array[1:]:
-            # Error handling for if result_column switches values to none
-            if line[query_column] == query_value and result_column != None:
-                result_column_string_accumulator.append(line[result_column])
+    try:
+        with open(file_name, 'r') as f:
+            lines_array = [line.split(',') for line in f]
+            # Skip header column
+            for line in lines_array[1:]:
+                # Error handling for if result_column switches values to none
+                if line[query_column] == query_value and result_column != None:
+                    result_column_string_accumulator.append(line[result_column])
 
-    result_column_int_list = [int(float(i)) for i in result_column_string_accumulator]
-    return result_column_int_list
+        result_column_int_list = [float_to_int(string_to_float(i)) for i in result_column_string_accumulator]
+        return result_column_int_list
+    except FileNotFoundError:
+        print(f"Error: No file with the name {file_name} found")
+        raise
+    except PermissionError:
+        print(f"Error: Permission denied for file {file_name}")
+        raise
+    
+def string_to_float(string):
+    """converts a string to a float
+    
+    Parameters
+    ----------
+    string : the string to convert
+    
+    Returns
+    -------
+    float
+        the string converted to a float
+    """
+    try:
+        return float(string)
+    except ValueError:
+        print(f"Error: {string} is not a number")
+        raise
+
+def float_to_int(float):
+    """converts a float to an int
+    
+    Parameters
+    ----------
+    float : the float to convert
+    
+    Returns
+    -------
+    int
+        the float converted to an int
+    """
+    try:
+        return int(float)
+    except ValueError:
+        print(f"Error: {float} is not a number")
+        raise
