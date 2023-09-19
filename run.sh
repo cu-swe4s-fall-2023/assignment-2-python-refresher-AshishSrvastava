3f9dc16119ced38c47f9a4d84ed15d8cd6f45582
Ashish Srivastava <fortcollinsashish@gmail.com>
2023-09-19 14:59:02 -0600

    added new working and failing examples in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
-# This works
-printf "This will work:\n"
-python print_fires.py --country 'United States of America' --file_name 'Agrofood_co2_emission.csv'
+# This works to get all types of fires
+python print_fires.py --country 'United States of America' --file_name 'Agrofood_co2_emission.csv' --all_fires True
+
+# This will work to get forest and savannah fires
+python print_fires.py --country 'United States of America' --file_name 'Agrofood_co2_emission.csv' --savannah_fires_column --forest_fires_column
 
 # This will throw an error
 printf "\nThis will throw an error:\n"
 python print_fires.py --country 'United States of America' --file_name 'Agrofood_co2_emission2.csv'
+
+# This will throw an error
+printf "This will give an error:\n"
+python print_fires.py --country 'United States of America' --file_name 'Agrofood_co2_emission.csv'
