f7cfc127cf26b84745d098fd80fbca05f69cf97d
Ashish Srivastava <fortcollinsashish@gmail.com>
2023-10-04 16:50:10 -0600

    added negative tests and fixed error handling for array operations

--- tests/unit_tests/test_my_utils.py
+++ tests/unit_tests/test_my_utils.py
@@ -7,20 +7,36 @@ class TestMyUtils(unittest.TestCase):
         expected_mean = 2
         self.assertEqual(my_utils.array_mean(input_array), expected_mean)
     
+    def test_non_array_mean(self):
+        input_non_array = "not an array"
+        self.assertRaises(TypeError, my_utils.array_mean, input_non_array)
+    
     def test_array_median(self):
         input_array = [1, 2, 3]
         expected_median = 2
         self.assertEqual(my_utils.array_median(input_array), expected_median)
     
+    def test_non_array_median(self):
+        input_non_array = "not an array"
+        self.assertRaises(TypeError, my_utils.array_median, input_non_array)
+    
     def test_array_variance(self):
         input_array = [1, 2, 3]
         expected_variance = 2/3
         self.assertEqual(my_utils.array_variance(input_array), expected_variance)
+        
+    def test_non_array_variance(self):
+        input_non_array = "not an array"
+        self.assertRaises(TypeError, my_utils.array_variance, input_non_array)
     
     def test_array_stdev(self):
         input_array = [1, 2, 3]
         expected_stdev = (2/3) ** 0.5
         self.assertEqual(my_utils.array_stdev(input_array), expected_stdev)
+        
+    def test_non_array_stdev(self):
+        input_non_array = "not an array"
+        self.assertRaises(TypeError, my_utils.array_stdev, input_non_array)
     
     def test_string_to_float(self):
         input_string = "15.3"
