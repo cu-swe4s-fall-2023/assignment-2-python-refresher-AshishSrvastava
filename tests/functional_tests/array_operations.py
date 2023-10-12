cd93195094603e65fde0940b686a18ac4ef6b944
Ashish Srivastava <fortcollinsashish@gmail.com>
2023-10-12 12:45:23 -0600

    refactored using pycodestyle, added style tests to test yml file

--- tests/functional_tests/array_operations.py
+++ tests/functional_tests/array_operations.py
@@ -1,14 +1,15 @@
 import sys
-sys.path.append('../../src')
+
+sys.path.append("../../src")
 import my_utils
 
 op = sys.argv[1]
-str_arr = sys.argv[2].split(',')
+str_arr = sys.argv[2].split(",")
 arr = [float(i) for i in str_arr]
 
-if op == 'mean':
+if op == "mean":
     print(my_utils.array_mean(arr))
-elif op == 'median':
+elif op == "median":
     print(my_utils.array_median(arr))
-elif op == 'stdev':
+elif op == "stdev":
     print(my_utils.array_stdev(arr))
